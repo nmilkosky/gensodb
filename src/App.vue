0a26faf4ec8d8204d98138530efc4ca56ca0b108
Nate Milkosky <nmilkosky@gmail.com>
2022-02-19 11:17:37 -0500

    ability box testing

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h1>Welcome to Your Vue.js App</h1>
+  <section class="section">
+    <div class="container">
+      <div class="ybox-active">Hso</div>
+      <br>
+      <div class="ybox-inactive">Hso</div>
+    </div>
+  </section>
 </template>
+
+<style scoped>
+.ybox-active {
+  padding-top: 4px;
+  width: 40px;
+  height: 40px;
+  border: 2px solid #bd9a00;
+  border-radius: 4px;
+  background-color: #ffe159; 
+  color: black;
+  text-align: center;
+}
+
+.ybox-inactive {
+  padding-top: 4px;
+  width: 40px;
+  height: 40px;
+  border: 2px solid rgb(49, 49, 49);
+  border-radius: 3px;
+  background-color: #9b8b46; 
+  color: white;
+  text-align: center;
+}
+</style>
