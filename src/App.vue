7c56f9ea7ab420d4b23af312a581cb347e478e2b
Nate Milkosky <nmilkosky@gmail.com>
2022-02-26 11:28:38 -0500

    add other ability boxes

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
       <div class="ybox-active">Hso</div>
       <br>
       <div class="ybox-inactive">Hso</div>
+      <br>
+      <div class="gbox-active">Bmb</div>
+      <br>
+      <div class="gbox-inactive">Bmb</div>
+      <br>
+      <div class="pbox-active">SAp</div>
+      <br>
+      <div class="pbox-inactive">SAp</div>
     </div>
   </section>
 </template>
@@ -13,7 +21,7 @@
   padding-top: 4px;
   width: 40px;
   height: 40px;
-  border: 2px solid #bd9a00;
+  border: 2px solid #6d5b0a;
   border-radius: 4px;
   background-color: #ffe159; 
   color: black;
@@ -24,9 +32,53 @@
   padding-top: 4px;
   width: 40px;
   height: 40px;
-  border: 2px solid rgb(49, 49, 49);
+  border: 2px solid rgb(77, 77, 77);
   border-radius: 3px;
-  background-color: #9b8b46; 
+  background-color: #7c7554; 
+  color: white;
+  text-align: center;
+}
+
+.gbox-active {
+  padding-top: 4px;
+  width: 40px;
+  height: 40px;
+  border: 2px solid #0a5f0e;
+  border-radius: 4px;
+  background-color: #17cc54; 
+  color: black;
+  text-align: center;
+}
+
+.gbox-inactive {
+  padding-top: 4px;
+  width: 40px;
+  height: 40px;
+  border: 2px solid rgb(77, 77, 77);
+  border-radius: 3px;
+  background-color: #547c54; 
+  color: white;
+  text-align: center;
+}
+
+.pbox-active {
+  padding-top: 4px;
+  width: 40px;
+  height: 40px;
+  border: 2px solid #6d0a44;
+  border-radius: 4px;
+  background-color: #ff59d5; 
+  color: black;
+  text-align: center;
+}
+
+.pbox-inactive {
+  padding-top: 4px;
+  width: 40px;
+  height: 40px;
+  border: 2px solid rgb(77, 77, 77);
+  border-radius: 3px;
+  background-color: #7c5472; 
   color: white;
   text-align: center;
 }
